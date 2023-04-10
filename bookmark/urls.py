9323d2588f209033dbb001a5bb4396ba99ca038c
isjmovo <isjmovo@gmail.com>
2023-04-10 10:29:50 +0900

    create view 생성 및 URL 설정

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView
+from bookmark.views import BookmarkListView, BookmarkCreateView
 
 urlpatterns = [
     path("", BookmarkListView.as_view(), name='list'),
+    path("add/", BookmarkCreateView.as_view(), name='add'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from bookmark.models import Bookmark
 
+# Create your views here.
+
 class BookmarkListView(ListView):
     model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_create'
